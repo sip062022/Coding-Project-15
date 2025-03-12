a1281cc29bc7c7beceb77396d55c0cf0600e186a
Sahara Sipple <sahara36@usf.edu>
2025-03-12 12:53:32 -0400

    Task 1 - Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,5 @@
+// Task 1: Creating the Base Structure //
+
+    const riskDashboard = document.getElementById('riskDashboard');  // gets elements from risk dashboard
+
+    console.log('Risk Dashboard Loaded');  // Logs message in console
